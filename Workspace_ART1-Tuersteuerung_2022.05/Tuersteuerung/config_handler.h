b4f934237d6afc737ca5dbd2cf95c1c13f0a7bd1
tobrtz <tobias.brantz@mailbox.tu-dresden>
2022-06-07 16:17:57 +0200

    bsp config

---
+++ Workspace_ART1-Tuersteuerung_2022.05/Tuersteuerung/config_handler.h
@@ -0,0 +1,14 @@
+//
+// Created by supertrooper on 07.06.2022.
+//
+
+#ifndef CONFIG_HANDLER_HH
+#define CONFIG_HANDLER_HH
+
+class config_handler {
+    public:
+        config_handler();
+        void run()
+};
+
+#endif // CONFIG_HANDLER_HH
