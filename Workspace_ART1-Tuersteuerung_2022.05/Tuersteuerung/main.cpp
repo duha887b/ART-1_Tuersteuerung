97a18164b9a992ac3f2beec54f463834443130bf
duha887b <dustin.hanusch@mailbox.tu-dresden.de>
2022-06-09 15:44:49 +0200

    nn

--- Workspace_ART1-Tuersteuerung_2022.05/Tuersteuerung/main.cpp
+++ Workspace_ART1-Tuersteuerung_2022.05/Tuersteuerung/main.cpp
@@ -3,22 +3,16 @@
 #include "test.h"
 #include "iostream"
 
-
 /* Das Programm in Eclipse mittels des Menüeintrags:
  * "Run -> External Tools" -> run in xterm"
  * starten!*/
 int main (int argc, char *argv[])
 {
-	// ... maybe insert your class initialization here?
-	// example:
+    // ... maybe insert your class initialization here?
+    // example:
 
-	//DoorControl control;
-	//control.run();
+    DoorControl control;
+    control.run();
 
-    Zustand st1(1);
-    Zustand st2(15);
-    st1.print();
-    st2.print();
-
-	return 0;
+    return 0;
 }
