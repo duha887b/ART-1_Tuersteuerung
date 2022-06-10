7f35f90fe3bd20019b2752c0c5bb56d8405e23ec
tobrtz <tobias.brantz@gmx.de>
2022-06-11 01:16:49 +0200

    cfg + cfg handler fertig (vermutich)

--- Workspace_ART1-Tuersteuerung_2022.05/Tuersteuerung/config_handler.cpp
+++ Workspace_ART1-Tuersteuerung_2022.05/Tuersteuerung/config_handler.cpp
@@ -13,7 +13,6 @@
 //config_handler(const config_handler&) = delete;				//nur Referenzierung, keine Kopie möglich
 //static config_handler* _instance;
 
-std::vector<std::vector> aktoren_sensoren_read;
 std::vector<HardwareElement> aktoren_sensoren_ini;
 
 config_handler::config_handler() {
@@ -24,12 +23,11 @@ static config_handler& get_instance() {
     //if (_instance==NULL) {
     //	_instance = new config_handler();
     //}
-
     static config_handler _instance;
     return _instance;
 }
 
-void config_handler::run() {
+std::vector<HardwareElement> config_handler::run() {
 
     std::string filename = "config.txt";
 
@@ -42,27 +40,62 @@ void config_handler::run() {
 
     std::string line;
     std::vector_tmp<std::string>;
+    std::vector<std::vector> aktoren_sensoren_read;
+
     i=0;
 
     while (std::getline(input, line)) {
         //std::cout << line << "/n";
+       // if ((line!="")&&i>23)             Fehler für logfile
+
         vector_tmp=split(line);
-        aktoren_sensoren_read.at(i);
-        i=++;
+        aktoren_sensoren_read.at(i)=vector_tmp;
+        i++;
     }
 
-    for (j=0;j<i-1;j++) {
+    int port=0;
+
+    for (j=0;j<24;j++) {              //24 -> 3 Ports mit je 8 pins
+
+        //if (((j+1)%8)==0)  port++;         //für die richtige Portzuweisung
+
         //sensor aktor initialisierung
+        if (aktoren_sensoren_read.at(j).at(0)=="reserviert"){
+            aktoren_sensoren_ini.at(j) = new HardwareElement(-1,-1,0);
+
+            continue;
+        }
+
+        std::string port_tmp = aktoren_sensoren_read.at(j).at(3);
+        std::string pin_tmp = aktoren_sensoren_read.at(j).at(4);
+        std::string opMode_tmp = aktoren_sensoren_read.at(j).at(2);
+
+        int i_port_tmp = std::stoi(port_tmp);
+        int i_pin_tmp = std::stoi(pin_tmp);
+        int i_opMode_tmp = std::stoi(opMode_tmp);
+
+        //if (i_port_tmp>2 || i_port_tmp<0) ; //Fehler für logfile
+
         if (aktoren_sensoren_read_sensoren_read.at(j).at(0)=="sensor") {
             //neuen sensor definieren
+
+            aktoren_sensoren_ini.at(j) = new Sensor(i_port_tmp,i_pin_tmp,0,i_opMode_tmp);
+
+            continue;
         }
+
         if (aktoren_sensoren_read.at(j).at(0)=="aktor") {
             //neuen aktor definieren
+
+            aktoren_sensoren_ini.at(j) = new Aktor(i_port_tmp,i_pin_tmp,0);
+
+            continue;
         }
-        else (//logfile für Fehler) {
+        else {       //logfile für Fehler
     }
 
-
     }
+
+    return aktoren_sensoren_ini;
 
 }
--- Workspace_ART1-Tuersteuerung_2022.05/Tuersteuerung/config_handler.h
+++ Workspace_ART1-Tuersteuerung_2022.05/Tuersteuerung/config_handler.h
@@ -5,10 +5,14 @@
 #ifndef CONFIG_HANDLER_HH
 #define CONFIG_HANDLER_HH
 
+#include "Aktor.h"
+#include "Sensor.h"
+#include "HardwareElement.h"
+
 class config_handler {
 
 public:
-    void run();
+    std::vector<HardwareElement> run();
     static config_handler& get_instance();
     config_handler();
 
